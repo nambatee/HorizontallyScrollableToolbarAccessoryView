519f91393c985fa77122fde6b06b83415290f18a
Natalia Harateh <nambatee@users.noreply.github.com>
2018-11-17 15:55:05 -0500

    Initial Commit

---
+++ Horizontally Scrollable Toolbar Accessory View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Horizontally Scrollable Toolbar Accessory View
+//
+//  Created by Natalia Harateh on 11/17/18.
+//  Copyright © 2018 Natalia Harateh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
